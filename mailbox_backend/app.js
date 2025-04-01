0fd9d74e9272042b2e3cebed2d79c16b59eef617
Sonika Deogharia <deoghariasonika@gmail.com>
2025-04-01 22:33:53 +0530

    compose email page

--- mailbox_backend/app.js
+++ mailbox_backend/app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./util/db");
 const authRoutes = require("./routes/userRoutes");
+const emailRoutes = require("./routes/emailRoutes");
 
 dotenv.config();
 connectDB();
@@ -13,6 +14,7 @@ app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/mails", emailRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
